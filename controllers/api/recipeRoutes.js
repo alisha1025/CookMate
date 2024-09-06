30634ff5e48ba02c3710239d624a39199927b584
alisha1025 <alishalakha96@gmail.com>
2024-09-05 20:51:17 -0400

    get routes commit

--- controllers/api/recipeRoutes.js
+++ controllers/api/recipeRoutes.js
@@ -1,6 +1,24 @@
 const router = require("express").Router();
-const { Recipe } = require("../../models");
+const { Recipe, User } = require("../models");
 
-// /api/recipes
+// Route to get all recipes
+router.get("/", async (req, res) => {
+  try {
+    // Fetch all recipes from the database
+    const recipeData = await Recipe.findAll({
+  
+    });
+
+    // Render the recipes view with the data
+    res.render("recipes", {
+      title: "All Recipes - CookMate",
+      recipes: recipeData.map(recipe => recipe.get({ plain: true })),
+      loggedIn: req.session.logged_in || false
+    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
