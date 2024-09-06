662d6fb3f2cb4a34542d206b4287f529e26f3f3d
MrGithubby <cybortey@gmail.com>
2024-09-05 20:52:50 -0400

    Recipe Routes

--- controllers/api/recipeRoutes.js
+++ controllers/api/recipeRoutes.js
@@ -1,6 +1,17 @@
 const router = require("express").Router();
 const { Recipe } = require("../../models");
 
-// /api/recipes
+// POST ROUTE /api/recipes
+router.post('/', async(req, res) => {
+    try {
+        const newRecipe = await Recipe.create({
+            ...req.body,
+            user_id: req.session.user_id,
+        })
+    res.status(200).json(newRecipe); 
+    } catch (err) {
+        res.status(400).json(err);
+    }
+})
 
 module.exports = router;
