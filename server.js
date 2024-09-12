34b79f502819f9f69e76c6ec1fd7569961cff771
Lain <ckisielius@gmail.com>
2024-09-12 15:42:40 -0400

    resetting tables to fix bug

--- server.js
+++ server.js
@@ -38,6 +38,6 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
 
 // leaving force on true will clear table data each time server starts
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
